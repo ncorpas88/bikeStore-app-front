203d12d0c574cd84961b1c06da58977b0a80926d
Natanael Corpas Rivero <natel1988@gmail.com>
2025-05-27 13:36:30 +0200

    sidebar mejorado

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -7,7 +7,7 @@ function Sidebar() {
       <Link to="/companyDetailsPage">
         <Button variant="dark">Companies</Button>
       </Link>
-      <img src="src/images/bikelogo.png" alt="bikelogo" width={100} />
+      <img src="src/images/bike.png" alt="bikelogo" width={100} />
       <Link to="/formCreateBikePage">
         <Button variant="dark">Create Bike</Button>
       </Link>

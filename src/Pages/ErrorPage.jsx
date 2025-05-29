b0998c876987b0e108396ae4499ed1419f41cdb9
Natanael Corpas Rivero <natel1988@gmail.com>
2025-05-29 15:00:38 +0200

    colores

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -4,9 +4,6 @@ function ErrorPage() {
   return (
     <div className="error">
       <h1>Error Page</h1>
-      <br />
-      <br />
-      <br />
       <img src="/images/bikeerror.png" alt="error" width={600}/>
     </div>
   );

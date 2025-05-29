6bf527a8fd96d1d91e1245eeeedd72e37916dd97
Natanael Corpas Rivero <natel1988@gmail.com>
2025-05-29 14:13:21 +0200

    errorpage

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -2,9 +2,12 @@
 
 function ErrorPage() {
   return (
-    <div>
-    
-      <img src="/images/bikeerror.png" alt="error" />
+    <div className="error">
+      <h1>Error Page</h1>
+      <br />
+      <br />
+      <br />
+      <img src="/images/bikeerror.png" alt="error" width={600}/>
     </div>
   );
 }
